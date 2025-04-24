da2adcde2edb28f050c94df4419d68da0b2bf4db
Carlos Aldana <noisycarlos@hey.com>
2025-04-23 21:23:10 -0700

    fixes wrong variable name in resolve script

--- resolve.sh
+++ resolve.sh
@@ -2,18 +2,18 @@
 
 app_name="DaVinci Resolve"
 
-installer=$(find . -maxdepth 1 -type f -name 'DaVinci_Resolve_Studio_*_Linux.run')
+installer_path=$(find . -maxdepth 1 -type f -name 'DaVinci_Resolve_Studio_*_Linux.run')
 
 if [ ! -f "$installer_path" ]; then
   echo "--- Skipping installation of ${app_name}, no installers found."
   exit 1
 fi
 
-version=$(echo ${installer} | sed -n 's/.\/DaVinci_Resolve_Studio_\([0-9.]*\)_Linux.run/\1/p')
+version=$(echo ${installer_path} | sed -n 's/.\/DaVinci_Resolve_Studio_\([0-9.]*\)_Linux.run/\1/p')
 echo "--- Installing ${app_name} version ${version}..."
 
 sudo apt install libapr1 libaprutil1 libxcb-cursor0 libxcb-damage0
-sudo SKIP_PACKAGE_CHECK=1 ${installer} -i -y
+sudo SKIP_PACKAGE_CHECK=1 ${installer_path} -i -y
 
 echo "--- Moving uneeded libraries..."
 
